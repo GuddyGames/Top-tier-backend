3049ed84df2cfbc64e755695cd8718499d143184
GuddyGames <guddygames310@gmail.com>
2026-09-25 21:17:31 +0100

    Avoid regex for proof URL validation

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -150,8 +150,18 @@ const getGlobalTrades = asyncHandler(async (req, res) => {
 const getPendingSubmissions = asyncHandler(async (req, res) => {
   const submissions = await TaskSubmission.listAllPending();
   for (const submission of submissions) {
-    if (submission.proof_url && !/^https?:\/\//i.test(submission.proof_url)) {
-      submission.proof_url = await createSignedUrl(submission.proof_url);
+    if (submission.proof_url) {
+      let isHttpUrl = false;
+      try {
+        const parsedUrl = new URL(submission.proof_url);
+        isHttpUrl = parsedUrl.protocol === 'http:' || parsedUrl.protocol === 'https:';
+      } catch {
+        isHttpUrl = false;
+      }
+
+      if (!isHttpUrl) {
+        submission.proof_url = await createSignedUrl(submission.proof_url);
+      }
     }
   }
   res.json({ submissions });
