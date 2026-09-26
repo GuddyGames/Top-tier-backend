f3ae7e5a80ced07b5e74411d49e4525709dbd045
GuddyGames <guddygames310@gmail.com>
2026-09-26 16:24:40 +0100

    Connect HTTP server to live market WebSocket

--- src/server.js
+++ src/server.js
@@ -5,16 +5,22 @@ if (!process.env.JWT_SECRET) {
   process.exit(1);
 }
 
+const http = require('http');
+const { WebSocketServer } = require('ws');
 const app = require('./app');
 const { scheduleDailyRankJob } = require('./jobs/dailyRankJob');
 const { schedulePriceTickJob } = require('./jobs/priceTickJob');
 const { configureTelegramWebhook } = require('./controllers/telegramController');
+const { startMarketFeed } = require('./services/marketFeed');
 
 const PORT = process.env.PORT || 5000;
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server, path: '/ws/market' });
 
-app.listen(PORT, () => {
+server.listen(PORT, async () => {
   console.log(`Leaderboard backend running on port ${PORT}`);
   scheduleDailyRankJob();
   schedulePriceTickJob();
   configureTelegramWebhook();
+  await startMarketFeed(wss);
 });
